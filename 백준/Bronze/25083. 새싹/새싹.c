c6c7ab88b2af7338dcb94b43b2d7dc88d799656f
skwwnl <57902567+skwwnl@users.noreply.github.com>
2022-08-25 16:54:23 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.c
@@ -0,0 +1,11 @@
+#include <stdio.h>  // Standard Input / Output (입/출력)
+
+int main(void) 
+{
+	printf("         ,r\'\"7\n");
+	printf("r`-_   ,\'  ,/\n");
+	printf(" \\. \". L_r\'\n");
+	printf("   `~\\/\n");
+	printf("      |\n");
+	printf("      |\n");
+}
