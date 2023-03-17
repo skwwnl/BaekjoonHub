36908dd0996a4ee231a2c49d1accb01781becfbf
skwwnl <skwwnl@gmail.com>
2023-03-18 06:29:42 +0900

    ss

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int[][] num1 = new int[a][b];
+        int[][] num2 = new int[a][b];
+
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                num1[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < a; i++){
+            for (int j = 0; j < b; j++){
+                num2[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < a; i++){
+            for (int j = 0; j < b; j++){
+                System.out.print(num1[i][j] + num2[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
