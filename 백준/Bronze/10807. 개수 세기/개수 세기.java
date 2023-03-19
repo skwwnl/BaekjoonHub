89eee4839242bf54225be6fac0e2cfd524b01fa6
skwwnl <57902567+skwwnl@users.noreply.github.com>
2023-03-20 05:10:16 +0900

    [Bronze V] Title: 개수 세기, Time: 128 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,22 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int num = sc.nextInt();
-        int[] totalNum = new int[num];
+        int num = Integer.parseInt(br.readLine());
+
+        int[] totalArray = new int[num];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         for (int i = 0; i < num; i++) {
-            int input = sc.nextInt();
-            totalNum[i] = input;
+            totalArray[i] = Integer.parseInt(st.nextToken());
         }
-
-        int findNum = sc.nextInt();
+        
+        int comNum = Integer.parseInt(br.readLine());
         int count = 0;
         for (int i = 0; i < num; i++) {
-            if(totalNum[i]==findNum)
+            if(totalArray[i]==comNum){
                 count++;
+            }
         }
         System.out.println(count);
     }
