936711caabdef6b7a6f90a907375b7e0ff84f302
skwwnl <57902567+skwwnl@users.noreply.github.com>
2024-08-09 14:07:24 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -0,0 +1,39 @@
+S = input()
+
+result = ""
+alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+]
+
+for i in alphabet:
+    if i in S:
+        result += str(S.index(i)) + " "
+    else:
+        result += "-1 "
+
+print(result)
