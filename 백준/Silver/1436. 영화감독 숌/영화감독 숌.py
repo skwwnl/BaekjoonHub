c8ff86444be64431d32f226255dfffcdfc612d8a
skwwnl <57902567+skwwnl@users.noreply.github.com>
2024-09-28 17:15:01 +0900

    [Silver V] Title: 영화감독 숌, Time: 560 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,12 @@
+n = int(input())
+cnt = 0
+result = 666
+
+while True:
+    if '666' in str(result):
+        cnt += 1
+    if cnt == n:
+        print(result)
+        break
+    
+    result += 1
